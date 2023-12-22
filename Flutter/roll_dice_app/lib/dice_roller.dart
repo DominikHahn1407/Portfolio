47cf3ec50f35c147c225fa01c02e5dae35381997
Dominik Hahn <dominik.hahn@studmail.htw-aalen.de>
2023-12-22 14:48:41 +0100

    Roll Dice App

---
+++ Flutter/roll_dice_app/lib/dice_roller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 6;
+
+  void rollDice() {
+    setState(() {
+      activeDiceImage = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$activeDiceImage.png',
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text("Roll Dice"),
+        )
+      ],
+    );
+  }
+}
