ce4170e29efd3134d65abfb6f71b6327394bd010
Dominik Hahn <dominik.hahn@studmail.htw-aalen.de>
2023-12-29 12:21:57 +0100

    Switch Screen implemented

---
+++ Flutter/quiz_app/lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.black54, Colors.black26],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
