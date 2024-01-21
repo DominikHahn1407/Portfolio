66a22a597d8ed3d3470b8f855d52a280cf99f927
Dominik Hahn <dominik.hahn@studmail.htw-aalen.de>
2024-01-21 14:22:12 +0100

    Voice Recognition Application

---
+++ Flutter/voice_recognition_app/lib/pages/home_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final SpeechToText _speechToText = SpeechToText();
+  String _spokenText = "";
+  double _confidenceLevel = 0;
+  bool _speechEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeech();
+  }
+
+  void initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {
+      _confidenceLevel = 0;
+    });
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(result) {
+    setState(() {
+      _spokenText = "${result.recognizedWords}";
+      _confidenceLevel = result.confidence;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Center(
+          child: Text(
+            "Speech Demo",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                _speechToText.isListening
+                    ? "Listening"
+                    : _speechEnabled
+                        ? "Tap the microphone to start listening"
+                        : "Speech is not available",
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  _spokenText,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ),
+            if (_speechToText.isNotListening && _confidenceLevel > 0)
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 100,
+                ),
+                child: Text(
+                  "Confidence: ${(_confidenceLevel * 100).toStringAsFixed(1)}%",
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _speechToText.isListening ? _stopListening : _startListening,
+        tooltip: 'Listen',
+        backgroundColor: Colors.red,
+        child: Icon(
+          _speechToText.isListening ? Icons.mic_off : Icons.mic,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
