7a11a48e31f2982dc7e66cab0af69cb6992860d2
Dominik Hahn <dominik.hahn@studmail.htw-aalen.de>
2024-01-25 19:02:27 +0100

    Expense Tracker Initialized

---
+++ Flutter/expense_tracker/lib/expenses.dart
@@ -0,0 +1,36 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: "Cinema",
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("The Chart"),
+          Text("Expenses"),
+        ],
+      ),
+    );
+  }
+}
