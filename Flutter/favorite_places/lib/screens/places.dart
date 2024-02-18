e1d80879039c8c5248a435b0a1c9a9e2363c47be
Dominik Hahn <dominik.hahn@studmail.htw-aalen.de>
2024-02-18 18:33:13 +0100

    Favorite Places App initialized and MT5 Python connection integrated

---
+++ Flutter/favorite_places/lib/screens/places.dart
@@ -0,0 +1,30 @@
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/screens/add_place.dart';
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Places"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: ((context) => const AddPlaceScreen())));
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: PlacesList(places: userPlaces),
+    );
+  }
+}
